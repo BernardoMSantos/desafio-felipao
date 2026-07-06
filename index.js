382c34f617f6a0cb44506d297c982c348f10e7c4
BernardoMSantos <moreirasantos11ms@outlook.com.br>
2026-07-05 22:26:46 -0300

    Primeira vesão do code

---
+++ index.js
@@ -0,0 +1,33 @@
+let nickname = "Kratos, O Bom de Guerra"
+let XP = 7043
+let rank 
+let inforPlayer = [nickname , XP , rank]
+
+switch(true) {
+    case inforPlayer[1] <= 1000:
+        inforPlayer[2] = "Ferro"
+        break
+    case inforPlayer[1] >= 1001 && inforPlayer[1] <= 2000:
+        inforPlayer[2] = "Bronze"
+        break
+    case inforPlayer[1] >= 2001 && inforPlayer[1] <= 5000:
+        inforPlayer[2] = "Prata"
+        break
+    case inforPlayer[1] >= 5001 && inforPlayer[1] <= 7000:
+        inforPlayer[2] = "Ouro"
+        break
+    case inforPlayer[1] >= 7001 && inforPlayer[1] <= 8000:
+        inforPlayer[2] = "Platina"
+        break
+    case inforPlayer[1] >= 8001 && inforPlayer[1] <= 9000:
+        inforPlayer[2] = "Ascendente"
+        break
+    case inforPlayer[1] >= 9001 && inforPlayer[1] <= 10000:
+        inforPlayer[2] = "Imortal"
+        break
+    case inforPlayer[1] >= 10001:
+        inforPlayer[2] = "Radiante"
+        break
+}
+
+console.log("O herói de nome" , inforPlayer[0] , "está no Rank" , inforPlayer[2])
